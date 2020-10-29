502d8842a516d554961d0122b2b6b4da30eb9e4c
Keith Chan <keithchan1218@gmail.com>
2020-10-29 17:32:41 +0800

    ver 1.0

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
+
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Axios from "axios";
+
+//Vue.config.productionTip = false;
+
+// set auth header
+Axios.defaults.headers.common["Authorization"] = `Bearer ${store.state.token}`;
+
 
 createApp(App)
   .use(store)
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import SignUp from "../views/SignUp.vue";
+import Login from "../views/Login.vue";
 
 const routes = [
   {
@@ -15,7 +17,17 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  },
+  {
+    path: "/sign-up",
+    name: "sign-up",
+    component: SignUp
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
 ];
 
 const router = createRouter({
